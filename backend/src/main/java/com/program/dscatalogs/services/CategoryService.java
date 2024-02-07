44c830e46b9bb00eb61d4af1290e8543e1ad2962
Felipe Steffan <felip.developer@hotmail.com>
2024-02-07 20:45:05 -0300

    Database seeding

--- backend/src/main/java/com/program/dscatalogs/services/CategoryService.java
+++ backend/src/main/java/com/program/dscatalogs/services/CategoryService.java
@@ -2,8 +2,10 @@ package com.program.dscatalogs.services;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.program.dscatalogs.entities.Category;
 import com.program.dscatalogs.repositories.CategoryRepository;
@@ -14,7 +16,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	
+	@Transactional(readOnly =  true)
 	public List<Category> findAll() {
 		return repository.findAll();
 		
